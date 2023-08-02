efe38313526efdeccb0c41f58d9d543e8be9fd74
corywilkie <52163034+corywilkie@users.noreply.github.com>
2023-08-02 14:33:19 -0300

    Create Spells.java

---
+++ Spells.java
@@ -0,0 +1,139 @@
+public class Spells {
+	public Spells() {
+	}
+	/*************************
+	  CLASS1 SPELLS (FIGHTER)
+	 *************************/
+	public String class1Name1(){
+		return ("name1");
+	}
+	public int class1Cost1(){
+		return 2;
+	}
+	public String class1Name2(){
+		return ("name2");
+        }
+        public int class1Cost2(){
+		return 3;
+        }
+        public String class1Name3(){
+		return ("name3");
+        }
+        public int class1Cost3(){
+		return 4;
+        }
+        public String class1Name4(){
+		return ("name4");
+        }
+        public int class1Cost4(){
+		return 5;
+        }
+        public String class1Name5(){
+		return ("name5");
+        }
+        public int class1Cost5(){
+		return 6;
+        }
+
+	/************************
+          CLASS2 SPELLS (CLERIC)
+         ************************/
+        public String class2Name1(){
+                return ("Cure Light Wounds");
+        }
+        public int class2Cost1(){
+                return 2;
+        }
+        public String class2Name2(){
+                return ("name2");
+        }
+        public int class2Cost2(){
+                return 3;
+        }
+        public String class2Name3(){
+                return ("name3");
+        }
+        public int class2Cost3(){
+                return 4;
+        }
+        public String class2Name4(){
+                return ("Cure Moderate Wounds");
+        }
+	public int class2Cost4(){
+                return 5;
+        }
+        public String class2Name5(){
+                return ("name5");
+        }
+        public int class2Cost5(){
+                return 6;
+        }
+
+	/***********************
+          CLASS3 SPELLS (THIEF)
+         ***********************/
+        public String class3Name1(){
+                return ("Hide");
+        }
+        public int class3Cost1(){
+                return 2;
+        }
+        public String class3Name2(){
+                return ("name2");
+        }
+        public int class3Cost2(){
+                return 3;
+        }
+        public String class3Name3(){
+                return ("Steal");
+        }
+        public int class3Cost3(){
+                return 4;
+        }
+        public String class3Name4(){
+                return ("name4");
+        }
+        public int class3Cost4(){
+                return 5;
+        }
+        public String class3Name5(){
+                return ("Stealth");
+        }
+        public int class3Cost5(){
+                return 6;
+        }
+
+	/****************************
+          CLASS4 SPELLS (MAGIC-USER)
+         ****************************/
+        public String class4Name1(){
+                return ("Magic Missile");
+        }
+        public int class4Cost1(){
+                return 2;
+        }
+        public String class4Name2(){
+                return ("name2");
+        }
+        public int class4Cost2(){
+                return 3;
+        }
+        public String class4Name3(){
+                return ("name3");
+        }
+        public int class4Cost3(){
+                return 4;
+        }
+        public String class4Name4(){
+                return ("name4");
+        }
+        public int class4Cost4(){
+                return 5;
+        }
+        public String class4Name5(){
+                return ("name5");
+        }
+        public int class4Cost5(){
+                return 6;
+        }
+}
