2c2ce80428a431097018049c2eb6140f75236f6b
corywilkie <52163034+corywilkie@users.noreply.github.com>
2023-08-02 14:31:13 -0300

    Create Equipment.java

---
+++ Equipment.java
@@ -0,0 +1,396 @@
+public class Equipment {
+	public Equipment(){
+	}
+	
+	/**************************
+	  CLASS1 WEAPONS (FIGHTER)
+	 **************************/
+	public String class1WeaponName1(){
+		return ("Fighter Weapon 1");
+	}
+	public int class1WeaponCost1(){
+		return 100;
+	}
+	public int class1WeaponDamage1(){
+		return 4;
+	}
+	public String class1WeaponName2(){
+                return ("Fighter Weapon 2");
+        }
+        public int class1WeaponCost2(){
+                return 1000;
+        }
+        public int class1WeaponDamage2(){
+                return 8;
+        }
+	public String class1WeaponName3(){
+                return ("Fighter Weapon 3");
+        }
+        public int class1WeaponCost3(){
+                return 10000;
+        }
+        public int class1WeaponDamage3(){
+                return 16;
+        }
+	public String class1WeaponName4(){
+                return ("Fighter Weapon 4");
+        }
+        public int class1WeaponCost4(){
+                return 100000;
+        }
+        public int class1WeaponDamage4(){
+                return 32;
+        }
+	public String class1WeaponName5(){
+                return ("Fighter Weapon 5");
+        }
+        public int class1WeaponCost5(){
+                return 1000000;
+        }
+        public int class1WeaponDamage5(){
+                return 64;
+        }
+
+	/************************
+	  CLASS1 ARMOR (FIGHTER)
+	 ************************/
+	public String class1ArmorName1(){
+                return ("Fighter Armor 1");
+        }
+        public int class1ArmorCost1(){
+                return 100;
+        }
+        public int class1ArmorDefense1(){
+                return 4;
+        }
+        public String class1ArmorName2(){
+                return ("Fighter Armor 2");
+        }
+        public int class1ArmorCost2(){
+                return 1000;
+        }
+        public int class1ArmorDefense2(){
+                return 8;
+        }
+        public String class1ArmorName3(){
+                return ("Fighter Armor 3");
+        }
+        public int class1ArmorCost3(){
+                return 10000;
+        }
+        public int class1ArmorDefense3(){
+                return 16;
+        }
+        public String class1ArmorName4(){
+                return ("Fighter Armor 4");
+        }
+        public int class1ArmorCost4(){
+                return 100000;
+        }
+        public int class1ArmorDefense4(){
+                return 32;
+        }
+        public String class1ArmorName5(){
+                return ("Fighter Armor 5");
+        }
+        public int class1ArmorCost5(){
+                return 1000000;
+        }
+        public int class1ArmorDefense5(){
+                return 64;
+        }
+
+	/*************************
+	  CLASS2 WEAPONS (CLERIC)
+	 *************************/
+	public String class2WeaponName1(){
+                return ("Cleric Weapon 1");
+        }
+        public int class2WeaponCost1(){
+                return 100;
+        }
+        public int class2WeaponDamage1(){
+                return 2;
+        }
+        public String class2WeaponName2(){
+                return ("Cleric Weapon 2");
+        }
+        public int class2WeaponCost2(){
+                return 1000;
+        }
+        public int class2WeaponDamage2(){
+                return 4;
+        }
+        public String class2WeaponName3(){
+                return ("Cleric Weapon 3");
+        }
+        public int class2WeaponCost3(){
+                return 10000;
+        }
+        public int class2WeaponDamage3(){
+                return 8;
+        }
+        public String class2WeaponName4(){
+                return ("Cleric Weapon 4");
+        }
+        public int class2WeaponCost4(){
+                return 100000;
+        }
+        public int class2WeaponDamage4(){
+                return 16;
+        }
+        public String class2WeaponName5(){
+                return ("Cleric Weapon 5");
+        }
+        public int class2WeaponCost5(){
+                return 1000000;
+        }
+        public int class2WeaponDamage5(){
+                return 32;
+        }
+
+	/***********************
+	  CLASS2 ARMOR (CLERIC)
+	 ***********************/
+	public String class2ArmorName1(){
+                return ("Cleric Armor 1");
+        }
+        public int class2ArmorCost1(){
+                return 100;
+        }
+        public int class2ArmorDefense1(){
+                return 4;
+        }
+        public String class2ArmorName2(){
+                return ("Cleric Armor 2");
+        }
+        public int class2ArmorCost2(){
+                return 1000;
+        }
+        public int class2ArmorDefense2(){
+                return 8;
+        }
+        public String class2ArmorName3(){
+                return ("Cleric Armor 3");
+        }
+        public int class2ArmorCost3(){
+                return 10000;
+        }
+        public int class2ArmorDefense3(){
+                return 16;
+        }
+        public String class2ArmorName4(){
+                return ("Cleric Armor 4");
+        }
+        public int class2ArmorCost4(){
+                return 100000;
+        }
+        public int class2ArmorDefense4(){
+                return 32;
+        }
+        public String class2ArmorName5(){
+                return ("Cleric Armor 5");
+        }
+        public int class2ArmorCost5(){
+                return 1000000;
+        }
+        public int class2ArmorDefense5(){
+                return 64;
+        }
+
+	/************************
+	  CLASS3 WEAPONS (THIEF)
+	 ************************/
+	public String class3WeaponName1(){
+                return ("Thief Weapon 1");
+        }
+        public int class3WeaponCost1(){
+                return 100;
+        }
+        public int class3WeaponDamage1(){
+                return 2;
+        }
+        public String class3WeaponName2(){
+                return ("Thief Weapon 2");
+        }
+        public int class3WeaponCost2(){
+                return 1000;
+        }
+        public int class3WeaponDamage2(){
+                return 4;
+        }
+        public String class3WeaponName3(){
+                return ("Thief Weapon 3");
+        }
+        public int class3WeaponCost3(){
+                return 10000;
+        }
+        public int class3WeaponDamage3(){
+                return 8;
+        }
+        public String class3WeaponName4(){
+                return ("Thief Weapon 4");
+        }
+        public int class3WeaponCost4(){
+                return 100000;
+        }
+        public int class3WeaponDamage4(){
+                return 16;
+        }
+        public String class3WeaponName5(){
+                return ("Thief Weapon 5");
+        }
+        public int class3WeaponCost5(){
+                return 1000000;
+        }
+        public int class3WeaponDamage5(){
+                return 32;
+        }
+
+	/**********************
+	  CLASS3 ARMOR (THIEF)
+	 **********************/
+	public String class3ArmorName1(){
+                return ("Thief Armor 1");
+        }
+        public int class3ArmorCost1(){
+                return 100;
+        }
+        public int class3ArmorDefense1(){
+                return 1;
+        }
+        public String class3ArmorName2(){
+                return ("Thief Armor 2");
+        }
+        public int class3ArmorCost2(){
+                return 1000;
+        }
+        public int class3ArmorDefense2(){
+                return 2;
+        }
+        public String class3ArmorName3(){
+                return ("Thief Armor 3");
+        }
+        public int class3ArmorCost3(){
+                return 10000;
+        }
+        public int class3ArmorDefense3(){
+                return 4;
+        }
+        public String class3ArmorName4(){
+                return ("Thief Armor 4");
+        }
+        public int class3ArmorCost4(){
+                return 100000;
+        }
+        public int class3ArmorDefense4(){
+                return 8;
+        }
+        public String class3ArmorName5(){
+                return ("Thief Armor 5");
+        }
+        public int class3ArmorCost5(){
+                return 1000000;
+        }
+        public int class3ArmorDefense5(){
+                return 16;
+        }
+
+	/*****************************
+	  CLASS4 WEAPONS (MAGIC-USER)
+	 *****************************/
+	public String class4WeaponName1(){
+                return ("Magic-User Weapon 1");
+        }
+        public int class4WeaponCost1(){
+                return 100;
+        }
+        public int class4WeaponDamage1(){
+                return 1;
+        }
+        public String class4WeaponName2(){
+                return ("Magic-User Weapon 2");
+        }
+        public int class4WeaponCost2(){
+                return 1000;
+        }
+        public int class4WeaponDamage2(){
+                return 2;
+        }
+        public String class4WeaponName3(){
+                return ("Magic-User Weapon 3");
+        }
+        public int class4WeaponCost3(){
+                return 10000;
+        }
+        public int class4WeaponDamage3(){
+                return 4;
+        }
+        public String class4WeaponName4(){
+                return ("Magic-User Weapon 4");
+        }
+        public int class4WeaponCost4(){
+                return 100000;
+        }
+        public int class4WeaponDamage4(){
+                return 8;
+        }
+        public String class4WeaponName5(){
+                return ("Magic-User Weapon 5");
+        }
+        public int class4WeaponCost5(){
+                return 1000000;
+        }
+        public int class4WeaponDamage5(){
+                return 16;
+        }
+
+	/***************************
+	  CLASS4 ARMOR (MAGIC-USER)
+	 ***************************/
+	public String class4ArmorName1(){
+                return ("Magic-User Armor 1");
+        }
+        public int class4ArmorCost1(){
+                return 100;
+        }
+        public int class4ArmorDefense1(){
+                return 1;
+        }
+        public String class4ArmorName2(){
+                return ("Magic-User Armor 2");
+        }
+        public int class4ArmorCost2(){
+                return 1000;
+        }
+        public int class4ArmorDefense2(){
+                return 2;
+        }
+        public String class4ArmorName3(){
+                return ("Magic-User Armor 3");
+        }
+        public int class4ArmorCost3(){
+                return 10000;
+        }
+        public int class4ArmorDefense3(){
+                return 4;
+        }
+        public String class4ArmorName4(){
+                return ("Magic-User Armor 4");
+        }
+        public int class4ArmorCost4(){
+                return 100000;
+        }
+        public int class4ArmorDefense4(){
+                return 8;
+        }
+        public String class4ArmorName5(){
+                return ("Magic-User Armor 5");
+        }
+        public int class4ArmorCost5(){
+                return 1000000;
+        }
+        public int class4ArmorDefense5(){
+                return 16;
+        }
+}
