6f28c332e63b14dde510d0f068b09d24b50ba7b7
corywilkie <52163034+corywilkie@users.noreply.github.com>
2023-08-02 14:32:30 -0300

    Create Monsters.java

---
+++ Monsters.java
@@ -0,0 +1,572 @@
+public class Monsters {
+	public Monsters(){
+	}
+	
+	/*****************
+	  MONSTERS PLAINS
+	 *****************/
+	public String monsterName1(){
+		return ("Suntail Hawk");
+	}
+	public int monsterEXP1(){
+		return 5;
+	}
+	public int monsterDamage1(){
+		return 1;
+	}
+	public int monsterHP1(){
+		return 5;
+	}
+	public int monsterGold1(){
+		return 5;
+	}
+	public String monsterName2(){
+                return ("Steve the Goblin");
+	}
+        public int monsterEXP2(){
+                return 10;
+        }
+        public int monsterDamage2(){
+                return 2;
+        }
+        public int monsterHP2(){
+                return 10;
+        }
+        public int monsterGold2(){
+		return 10;
+	}
+	public String monsterName3(){
+                return ("Tom the Orc");
+	}
+        public int monsterEXP3(){
+                return 15;
+        }
+        public int monsterDamage3(){
+                return 3;
+        }
+        public int monsterHP3(){
+                return 15;
+        }
+        public int monsterGold3(){
+		return 15;
+	}
+	public String monsterName4(){
+                return ("Chuck the Buck");
+	}
+        public int monsterEXP4(){
+                return 20;
+        }
+        public int monsterDamage4(){
+                return 4;
+        }
+        public int monsterHP4(){
+                return 20;
+        }
+        public int monsterGold4(){
+		return 20;
+	}
+	public String monsterName5(){
+                return ("Fat Albert");
+	}
+        public int monsterEXP5(){
+                return 25;
+        }
+        public int monsterDamage5(){
+                return 5;
+        }
+        public int monsterHP5(){
+                return 25;
+        }
+        public int monsterGold5(){
+		return 25;
+	}
+
+	/*****************
+	  MONSTERS FOREST
+	 *****************/
+	public String monsterName6(){
+                return ("Fat Albert");
+	}
+        public int monsterEXP6(){
+                return 25;
+        }
+        public int monsterDamage6(){
+                return 5;
+        }
+        public int monsterHP6(){
+                return 25;
+        }
+        public int monsterGold6(){
+		return 25;
+	}
+	public String monsterName7(){
+                return ("Fat Albert");
+	}
+        public int monsterEXP7(){
+                return 25;
+        }
+        public int monsterDamage7(){
+                return 5;
+        }
+        public int monsterHP7(){
+                return 25;
+        }
+        public int monsterGold7(){
+		return 25;
+	}
+	public String monsterName8(){
+                return ("Fat Albert");
+	}
+        public int monsterEXP8(){
+                return 25;
+        }
+        public int monsterDamage8(){
+                return 5;
+        }
+        public int monsterHP8(){
+                return 25;
+        }
+        public int monsterGold8(){
+		return 25;
+	}
+	public String monsterName9(){
+                return ("Fat Albert");
+	}
+        public int monsterEXP9(){
+                return 25;
+        }
+        public int monsterDamage9(){
+                return 5;
+        }
+        public int monsterHP9(){
+                return 25;
+        }
+        public int monsterGold9(){
+		return 25;
+	}
+	public String monsterName10(){
+                return ("Fat Albert");
+	}
+        public int monsterEXP10(){
+                return 25;
+        }
+        public int monsterDamage10(){
+                return 5;
+        }
+        public int monsterHP10(){
+                return 25;
+        }
+        public int monsterGold10(){
+		return 25;
+	}
+
+	/****************
+	  MONSTERS SWAMP
+	 ****************/
+	public String monsterName11(){
+                return ("Fat Albert");
+	}
+        public int monsterEXP11(){
+                return 25;
+        }
+        public int monsterDamage11(){
+                return 5;
+        }
+        public int monsterHP11(){
+                return 25;
+        }
+        public int monsterGold11(){
+		return 25;
+	}
+	public String monsterName12(){
+                return ("Fat Albert");
+	}
+        public int monsterEXP12(){
+                return 25;
+        }
+        public int monsterDamage12(){
+                return 5;
+        }
+        public int monsterHP12(){
+                return 25;
+        }
+        public int monsterGold12(){
+		return 25;
+	}
+	public String monsterName13(){
+                return ("Fat Albert");
+	}
+        public int monsterEXP13(){
+                return 25;
+        }
+        public int monsterDamage13(){
+                return 5;
+        }
+        public int monsterHP13(){
+                return 25;
+        }
+        public int monsterGold13(){
+		return 25;
+	}
+	public String monsterName14(){
+                return ("Fat Albert");
+	}
+        public int monsterEXP14(){
+                return 25;
+        }
+        public int monsterDamage14(){
+                return 5;
+        }
+        public int monsterHP14(){
+                return 25;
+        }
+        public int monsterGold14(){
+		return 25;
+	}
+	public String monsterName15(){
+                return ("Fat Albert");
+	}
+        public int monsterEXP15(){
+                return 25;
+        }
+        public int monsterDamage15(){
+                return 5;
+        }
+        public int monsterHP15(){
+                return 25;
+        }
+        public int monsterGold15(){
+		return 25;
+	}
+
+	/******************
+	  MONSTERS ISLANDS
+	 ******************/
+	public String monsterName16(){
+                return ("Fat Albert");
+	}
+        public int monsterEXP16(){
+                return 25;
+        }
+        public int monsterDamage16(){
+                return 5;
+        }
+        public int monsterHP16(){
+                return 25;
+        }
+        public int monsterGold16(){
+		return 25;
+	}
+	public String monsterName17(){
+                return ("Fat Albert");
+	}
+        public int monsterEXP17(){
+                return 25;
+        }
+        public int monsterDamage17(){
+                return 5;
+        }
+        public int monsterHP17(){
+                return 25;
+        }
+        public int monsterGold17(){
+		return 25;
+	}
+	public String monsterName18(){
+                return ("Fat Albert");
+	}
+        public int monsterEXP18(){
+                return 25;
+        }
+        public int monsterDamage18(){
+                return 5;
+        }
+        public int monsterHP18(){
+                return 25;
+        }
+        public int monsterGold18(){
+		return 25;
+	}
+	public String monsterName19(){
+                return ("Fat Albert");
+	}
+        public int monsterEXP19(){
+                return 25;
+        }
+        public int monsterDamage19(){
+                return 5;
+        }
+        public int monsterHP19(){
+                return 25;
+        }
+        public int monsterGold19(){
+		return 25;
+	}
+	public String monsterName20(){
+                return ("Fat Albert");
+	}
+        public int monsterEXP20(){
+                return 25;
+        }
+        public int monsterDamage20(){
+                return 5;
+        }
+        public int monsterHP20(){
+                return 25;
+        }
+        public int monsterGold20(){
+		return 25;
+	}
+
+	/*******************
+	  MONSTERS MOUTAINS
+	 *******************/
+	public String monsterName21(){
+                return ("Fat Albert");
+	}
+        public int monsterEXP21(){
+                return 25;
+        }
+        public int monsterDamage21(){
+                return 5;
+        }
+        public int monsterHP21(){
+                return 25;
+        }
+        public int monsterGold21(){
+		return 25;
+	}
+	public String monsterName22(){
+                return ("Fat Albert");
+	}
+        public int monsterEXP22(){
+                return 25;
+        }
+        public int monsterDamage22(){
+                return 5;
+        }
+        public int monsterHP22(){
+                return 25;
+        }
+        public int monsterGold22(){
+		return 25;
+	}
+	public String monsterName23(){
+                return ("Fat Albert");
+	}
+        public int monsterEXP23(){
+                return 25;
+        }
+        public int monsterDamage23(){
+                return 5;
+        }
+        public int monsterHP23(){
+                return 25;
+        }
+        public int monsterGold23(){
+		return 25;
+	}
+	public String monsterName24(){
+                return ("Fat Albert");
+	}
+        public int monsterEXP24(){
+                return 25;
+        }
+        public int monsterDamage24(){
+                return 5;
+        }
+        public int monsterHP24(){
+                return 25;
+        }
+        public int monsterGold24(){
+		return 25;
+	}
+	public String monsterName25(){
+                return ("Fat Albert");
+	}
+        public int monsterEXP25(){
+                return 25;
+        }
+        public int monsterDamage25(){
+                return 5;
+        }
+        public int monsterHP25(){
+                return 25;
+        }
+        public int monsterGold25(){
+		return 25;
+	}
+	
+	/*****************
+	  MONSTERS PORTAL
+	 *****************/
+	public String monsterName26(){
+                return ("Fat Albert");
+	}
+        public int monsterEXP26(){
+                return 25;
+        }
+        public int monsterDamage26(){
+                return 5;
+        }
+        public int monsterHP26(){
+                return 25;
+        }
+        public int monsterGold26(){
+		return 25;
+	}
+	public String monsterName27(){
+                return ("Fat Albert");
+	}
+        public int monsterEXP27(){
+                return 25;
+        }
+        public int monsterDamage27(){
+                return 5;
+        }
+        public int monsterHP27(){
+                return 25;
+        }
+        public int monsterGold27(){
+		return 25;
+	}
+	public String monsterName28(){
+                return ("Fat Albert");
+	}
+        public int monsterEXP28(){
+                return 25;
+        }
+        public int monsterDamage28(){
+                return 5;
+        }
+        public int monsterHP28(){
+                return 25;
+        }
+        public int monsterGold28(){
+		return 25;
+	}
+	public String monsterName29(){
+                return ("Fat Albert");
+	}
+        public int monsterEXP29(){
+                return 25;
+        }
+        public int monsterDamage29(){
+                return 5;
+        }
+        public int monsterHP29(){
+                return 25;
+        }
+        public int monsterGold29(){
+		return 25;
+	}
+	public String monsterName30(){
+                return ("Fat Albert");
+	}
+        public int monsterEXP30(){
+                return 25;
+        }
+        public int monsterDamage30(){
+                return 5;
+        }
+        public int monsterHP30(){
+                return 25;
+        }
+        public int monsterGold30(){
+		return 25;
+	}
+
+	/********
+	  BOSSES
+	 ********/
+	public String bossName1(){
+                return ("Cory The Mac Daddy");
+        }
+        public int bossGold1(){
+                return 100;
+        }
+        public int bossEXP1(){
+                return 500;
+        }
+        public int bossDamage1(){
+                return 5;
+        }
+        public int bossHP1(){
+                return 50;
+        }
+	public String bossName2(){
+                return ("Robert The Dark Lord Of The Underworld");
+        }
+	public int bossGold2(){
+		return 500;
+	}
+        public int bossEXP2(){
+                return 2000;
+        }
+        public int bossDamage2(){
+                return 10;
+        }
+        public int bossHP2(){
+                return 200;
+        }
+	public String bossName3(){
+                return ("Cory The Mac Daddy");
+        }
+        public int bossGold3(){
+                return 100;
+        }
+        public int bossEXP3(){
+                return 500;
+        }
+        public int bossDamage3(){
+                return 5;
+        }
+        public int bossHP3(){
+                return 50;
+        }
+	public String bossName4(){
+                return ("Robert The Dark Lord Of The Underworld");
+        }
+	public int bossGold4(){
+		return 500;
+	}
+        public int bossEXP4(){
+                return 2000;
+        }
+        public int bossDamage4(){
+                return 10;
+        }
+        public int bossHP4(){
+                return 200;
+        }
+	public String bossName5(){
+                return ("Cory The Mac Daddy");
+        }
+        public int bossGold5(){
+                return 100;
+        }
+        public int bossEXP5(){
+                return 500;
+        }
+        public int bossDamage5(){
+                return 5;
+        }
+        public int bossHP5(){
+                return 50;
+        }
+	public String bossName6(){
+                return ("Robert The Dark Lord Of The Underworld");
+        }
+	public int bossGold6(){
+		return 500;
+	}
+        public int bossEXP6(){
+                return 2000;
+        }
+        public int bossDamage6(){
+                return 10;
+        }
+        public int bossHP6(){
+                return 200;
+        }
+}
